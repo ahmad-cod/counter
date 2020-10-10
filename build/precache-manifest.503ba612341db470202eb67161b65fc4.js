855c05dedc6bd55e6646d289e20aa9b914162e51
aroyehunahmad <ahmadadewale800@gmail.com>
2020-10-10 10:29:47 -0700

    removed build from gitignore

---
+++ build/precache-manifest.503ba612341db470202eb67161b65fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d2283e4c71ea1c8ac85c11957c228a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc65033405121792111",
+    "url": "/static/css/main.d83bce3a.chunk.css"
+  },
+  {
+    "revision": "318ca5d6af7e806d4f70",
+    "url": "/static/js/2.ba48adbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba48adbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc65033405121792111",
+    "url": "/static/js/main.54fdcd37.chunk.js"
+  },
+  {
+    "revision": "2fa9e31451af5fbb06a3",
+    "url": "/static/js/runtime-main.c51d566f.js"
+  }
+]);
